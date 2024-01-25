0b5902af50253da8d9236dffda5919a6dd110bcb
Sahan2000 <udanasahan1@gmail.com>
2024-01-25 11:11:09 +0530

    set customer ids combo box

--- controller/order.js
+++ controller/order.js
@@ -1,15 +1,32 @@
 import {OrderApi} from "../api/orderApi.js";
 import {OrderModel} from "../model/OrderModel.js";
+import {CustomerApi} from "../api/customerApi.js";
 
 let orderPage = $('#order_page');
 
 let orderId = $('#orderId');
 
+let customerIdCb = $('#custId');
+
 let orderApi = new OrderApi();
+let customerApi = new CustomerApi();
 
 orderPage.eq(0).on('click',function (){
      generateOrderId();
+     getAllCustomerId();
 });
+
+function getAllCustomerId(){
+     customerApi.getAllCustomer().then((r)=>{
+          customerIdCb.find("option:not(:first-child").removeClass(1);
+          r.forEach((customer)=>{
+               customerIdCb.append($("<option>",{
+                    value:customer.customerId,
+                    text:customer.customerId
+               }))
+          })
+     })
+}
 
 function generateOrderId(){
      orderApi.generateOrderId().then(r => {
